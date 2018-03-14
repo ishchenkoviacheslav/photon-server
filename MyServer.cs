0d0c5161b4a194ef81c215e3dc795d9875404ce8
Ищенко Вячеслав <ishchenkoviacheslav@live.com>
2018-03-14 11:21:38 +0200

    photon server first start

---
+++ MyServer.cs
@@ -0,0 +1,38 @@
+﻿using ExitGames.Logging;
+using ExitGames.Logging.Log4Net;
+using log4net.Config;
+using Photon.SocketServer;
+using System.IO;
+
+
+namespace TestPhotonLib
+{
+    public class MyServer : ApplicationBase
+    {
+        private readonly ILogger Log = LogManager.GetCurrentClassLogger();
+
+        protected override PeerBase CreatePeer(InitRequest initRequest)
+        {
+            return new UnityClient(initRequest);
+        }
+
+        protected override void Setup()
+        {
+            var file = new FileInfo(Path.Combine(BinaryPath, "log4net.config"));
+            if(file.Exists)
+            {
+                LogManager.SetLoggerFactory(Log4NetLoggerFactory.Instance);
+                XmlConfigurator.ConfigureAndWatch(file);
+            }
+
+            Log.Info("Server is ready!");
+
+
+        }
+
+        protected override void TearDown()
+        {
+            Log.Debug("Server is stoped!");
+        }
+    }
+}
---
+++ UnityClient.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using ExitGames.Logging;
+using Photon.SocketServer;
+using PhotonHostRuntimeInterfaces;
+
+namespace TestPhotonLib
+{
+    public class UnityClient : ClientPeer
+    {
+        private readonly ILogger Log = LogManager.GetCurrentClassLogger();
+
+        public UnityClient(InitRequest initRequest) : base(initRequest)
+        {
+            Log.Info("Player connection ip:" + initRequest.RemoteIP);
+        }
+
+        protected override void OnDisconnect(DisconnectReason reasonCode, string reasonDetail)
+        {
+            Log.Debug("Disconnected");
+        }
+
+        protected override void OnOperationRequest(OperationRequest operationRequest, SendParameters sendParameters)
+        {
+            switch (operationRequest.OperationCode)
+            {
+                case 1:
+                    if (operationRequest.Parameters.ContainsKey(1))
+                    {
+                        Log.Debug("rect: " + operationRequest.Parameters[1]);
+                        OperationResponse response = new OperationResponse(operationRequest.OperationCode)
+                        {
+                            Parameters = new Dictionary<byte, object> { { 1, "response message" } }
+                        };
+                        SendOperationResponse(response, sendParameters);
+                    }
+                    break;
+                case 2:
+                    if (operationRequest.Parameters.ContainsKey(1))
+                    {
+                        Log.Debug("rect: " + operationRequest.Parameters[1]);
+                        EventData eventData = new EventData(1)
+                        {
+                            Parameters = new Dictionary<byte, object> {{1, "response for event"}}
+                        };
+                        SendEvent(eventData, sendParameters);
+                    }
+                    break;
+                default:
+                    Log.Debug("Unknown OperationRequest recieved!:"+ operationRequest.OperationCode);
+                    break;
+                    
+            }
+        }
+    }
+}
